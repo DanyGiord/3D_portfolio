a5962e40977e0528e696d7d0a4c16c03ed2cb822
DanyGiord <danygiord2@gmail.com>
2023-12-25 00:46:03 +0100

    fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a Junior Front-End developer with experience in
+        I'm a Front-End developer with experience in
         JavaScript, and expertise in frameworks like React, and
         Next.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
